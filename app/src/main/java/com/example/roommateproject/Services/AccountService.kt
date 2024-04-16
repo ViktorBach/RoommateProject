f356d98496a946b90a4df9ccf7409d951fe96be0
ViktorBach <vmb0107@gmail.com>
2024-04-16 15:44:20 +0200

    added login functionality

--- app/src/main/java/com/example/roommateproject/Services/AccountService.kt
+++ app/src/main/java/com/example/roommateproject/Services/AccountService.kt
@@ -20,28 +20,14 @@ class AccountService {
     }
 
     fun login(email: String, password: String, onResult: (Boolean, String?) -> Unit) {
-        // Check if the user exists before attempting to sign in
-        Firebase.auth.fetchSignInMethodsForEmail(email)
-            .addOnSuccessListener { result ->
-                if (result.signInMethods!!.contains(EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD)) {
-                    // User exists, attempt to sign in
-                    Firebase.auth.signInWithEmailAndPassword(email, password)
-                        .addOnSuccessListener {
-                            // Login successful
-                            onResult(true, null)
-                        }
-                        .addOnFailureListener { exception ->
-                            // Login failed
-                            val errorMessage = exception.message
-                            onResult(false, errorMessage)
-                        }
-                } else {
-                    // User does not exist, return failure
-                    onResult(false, "User does not exist. Please register.")
-                }
+        // Attempt to sign in the user
+        Firebase.auth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                // Login successful
+                onResult(true, null)
             }
             .addOnFailureListener { exception ->
-                // Error occurred while checking for existing user
+                // Login failed
                 val errorMessage = exception.message
                 onResult(false, errorMessage)
             }
