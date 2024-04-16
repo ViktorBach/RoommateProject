2b82f8220ac4aafd8186e9d63348ed13edaf7edb
SofieAmalie44 <sofieamaliethorlund@gmail.com>
2024-04-16 13:05:06 +0200

    First commit & push for design changes in Register file. Test.

--- app/src/main/java/com/example/roommateproject/Register/Register.kt
+++ app/src/main/java/com/example/roommateproject/Register/Register.kt
@@ -2,23 +2,63 @@ package com.example.roommateproject.Register
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Register (goToFrontPage: () -> Unit) {
-    Box(
-        Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Column {
-            Text(text = "Welcome")
-            Button(onClick = goToFrontPage ) {
-                Text(text = "Login")
+
+    var text by remember { mutableStateOf("") }
+
+    Box{
+        Text(text = "Home")
+
+        Column (
+            Modifier.padding(100.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome home")
+
+            TextField(
+                value = text,
+                onValueChange = { newText ->
+                    text = newText
+                },
+                label = { Text("Enter text") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+            TextField(
+                value = text,
+                onValueChange = { newText ->
+                    text = newText
+                },
+                label = { Text("Enter text") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+            Row {
+                Button(onClick = goToFrontPage ) {
+                    Text(text = "Register")
+                }
+                Button(onClick = goToFrontPage ) {
+                    Text(text = "Login")
+                }
             }
         }
     }
