09101c2de4e1987b816d96f6ffdeb956dafe5ab0
natazja <natazjadahl@gmail.com>
2024-04-22 19:37:01 +0200

    nat: type.kt + register setup

--- app/src/main/java/com/example/roommateproject/Register/Register.kt
+++ app/src/main/java/com/example/roommateproject/Register/Register.kt
@@ -1,74 +1,197 @@
 package com.example.roommateproject.Register
 
+import android.content.res.Resources
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roommateproject.ui.theme.Typography
+import com.example.roommateproject.ui.theme.lightBlue
+import com.example.roommateproject.ui.theme.lightYellow
+import com.example.roommateproject.ui.theme.orange
+
 
 @Composable
 fun Register (navigateRoomLogin: () -> Unit) {
 
     val registerViewModel = viewModel<RegisterViewModel>()
 
-    Box{
-        Text(text = "Home")
-
-        Column (
-            Modifier.padding(100.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Welcome home")
-
-            TextField(
-                value = registerViewModel.email,
-                onValueChange = {
-                    registerViewModel.onEmailChange(it)
-                },
-                label = { Text("Enter email") },
+    Column {
+        Spacer(modifier = Modifier.width(16.dp))
+        Row {
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-            )
-            TextField(
-                value = registerViewModel.password,
-                onValueChange = {
-                    registerViewModel.onPasswordChange(it)
-                },
-                label = { Text("Enter password") },
+                    .background(Color.White)
+                    .fillMaxHeight(0.1f),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Text(
+                    text = "H",
+                    style = Typography.titleLarge,
+                    color = orange
+                )
+            }
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-            )
-            TextField(
-                value = registerViewModel.username,
-                onValueChange = {
-                    registerViewModel.onUsernameChange(it)
-                },
-                label = { Text("Enter username") },
+                    .background(Color.White)
+                    .fillMaxHeight(0.115f),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Text(
+                    text = "omie",
+                    style = Typography.titleMedium,
+                    color = orange
+                )
+            }
+        }
+        Row {
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-            )
-            Row {
-                Button(onClick = { registerViewModel.registerNewUser(navigateRoomLogin) }) {
-                    Text(text = "Register")
+                    .background(Color.White)
+                    .fillMaxHeight(0.25f)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Welcome Homie",
+                    style = Typography.titleSmall,
+                    color = lightYellow
+                )
+            }
+        }
+        Row {
+            Box(
+                modifier = Modifier
+                    .background(Color.White)
+                    .fillMaxHeight(0.15f)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                TextField(
+                    value = registerViewModel.email,
+                    onValueChange = {
+                        registerViewModel.onEmailChange(it)
+                    },
+                    label = {
+                        Text(
+                            "Enter e-mail",
+                            style = Typography.labelMedium,
+                            color = lightBlue
+                        )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                )
+            }
+        }
+        Row {
+            Box(
+                modifier = Modifier
+                    .background(Color.White)
+                    .fillMaxHeight(0.15f)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                TextField(
+                    value = registerViewModel.password,
+                    onValueChange = {
+                        registerViewModel.onPasswordChange(it)
+                    },
+                    label = {
+                        Text(
+                            "Enter password",
+                            style = Typography.labelMedium,
+                            color = lightBlue
+                        )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                )
+            }
+        }
+        Row {
+            Box(
+                modifier = Modifier
+                    .background(Color.White)
+                    .fillMaxHeight(0.2f)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                TextField(
+                    value = registerViewModel.username,
+                    onValueChange = {
+                        registerViewModel.onUsernameChange(it)
+                    },
+                    label = {
+                        Text(
+                            "Enter username",
+                            style = Typography.labelMedium,
+                            color = lightBlue
+                        )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                )
+            }
+        }
+        Row {
+            Box(
+                modifier = Modifier
+                    .background(Color.White)
+                    .fillMaxHeight(0.75f)
+                    .fillMaxWidth(0.5f),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(
+                    onClick = { registerViewModel.registerNewUser(navigateRoomLogin) },
+                ) {
+                    Text(text = "Register",
+                        style = Typography.labelMedium,
+                        color = lightBlue)
                 }
-                Button(onClick = { registerViewModel.loginWithUser(navigateRoomLogin) }) {
-                    Text(text = "Login")
+            }
+            Box(
+                modifier = Modifier
+                    .background(Color.White)
+                    .fillMaxHeight(0.75f)
+                    .fillMaxWidth(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(
+                    onClick = { registerViewModel.loginWithUser(navigateRoomLogin) }
+                ) {
+                    Text(text = "Login",
+                        style = Typography.labelMedium,
+                        color = lightBlue)
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun RegisterPreview () {
--- app/src/main/java/com/example/roommateproject/ui/theme/Type.kt
+++ app/src/main/java/com/example/roommateproject/ui/theme/Type.kt
@@ -1,5 +1,4 @@
 package com.example.roommateproject.ui.theme
-
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -8,27 +7,75 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    // Large body text
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
+    ),
+    // Medium body text
+    bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    // Small body text
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    // Large title text
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 52.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 2.sp
+    ),
+    // Medium title text
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 38.sp,
+        lineHeight = 38.sp,
+        letterSpacing = 0.sp,
+    ),
+    // Small title text
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp
+    ),
+    // Large label text
+    labelLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    // Small label text
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.sp
     )
-    */
 )
