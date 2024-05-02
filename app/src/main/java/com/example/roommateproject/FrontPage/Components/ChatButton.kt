a3f8a3bf1427626d1fd83a574242fbdc89976922
BirkLauritzen <birklauritzen@gmail.com>
2024-05-02 12:06:28 +0200

    Create ChatButton.kt

---
+++ app/src/main/java/com/example/roommateproject/FrontPage/Components/ChatButton.kt
@@ -0,0 +1,44 @@
+package com.example.roommateproject.FrontPage.Components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.roommateproject.ui.theme.Typography
+import com.example.roommateproject.ui.theme.jaldiBoldFontFamily
+import com.example.roommateproject.ui.theme.lightYellow
+import com.example.roommateproject.ui.theme.white
+
+@Composable
+fun ChatButton() {
+    Box(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxHeight(0.2f)
+            .fillMaxWidth(0.99f),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = {}, //Insert pop-up with chat
+            colors = ButtonDefaults.buttonColors(
+                lightYellow // Set the text color to lightYellow
+            ), modifier = Modifier.padding(end = 8.dp)
+        ) {
+            Text(
+                text = "Chat",
+                fontFamily = jaldiBoldFontFamily,
+                style = Typography.labelSmall,
+                color = white
+            )
+        }
+    }
+}
