2f6e5dbee7cc88d8ef4bf20170351ddd2aae33d2
BirkLauritzen <birklauritzen@gmail.com>
2024-05-02 15:03:38 +0200

    Create EnterPasswordInput.kt

---
+++ app/src/main/java/com/example/roommateproject/Register/Components/EnterPasswordInput.kt
@@ -0,0 +1,19 @@
+package com.example.roommateproject.Register.Components
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roommateproject.Register.BoxLayout
+import com.example.roommateproject.Register.RegisterViewModel
+
+@Composable
+fun EnterPasswordInput (navigateRoomLogin: () -> Unit) {
+
+    val registerViewModel = viewModel<RegisterViewModel>()
+
+    BoxLayout(
+        value = registerViewModel.password,
+        onValueChange = { newValue -> registerViewModel.onPasswordChange(newValue) },
+        labelText = "Enter password",
+        height = 0.18f
+    )
+}
