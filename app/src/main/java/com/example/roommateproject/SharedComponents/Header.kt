5a617650bf89dcc578253714d2ab7a6d0d75a73d
BirkLauritzen <birklauritzen@gmail.com>
2024-05-02 12:05:53 +0200

    Create Header.kt

---
+++ app/src/main/java/com/example/roommateproject/SharedComponents/Header.kt
@@ -0,0 +1,53 @@
+package com.example.roommateproject.SharedComponents
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.roommateproject.ui.theme.orange
+import com.example.roommateproject.ui.theme.karantinaFontFamily
+import com.example.roommateproject.ui.theme.katibehFontFamily
+import com.example.roommateproject.ui.theme.Typography
+
+@Composable
+fun Header() {
+    Spacer(modifier = Modifier.width(16.dp))
+    Row {
+        Box(
+            modifier = Modifier
+                .background(Color.White)
+                .fillMaxHeight(0.1f),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Text(
+                text = "H",
+                fontFamily = karantinaFontFamily,
+                style = Typography.titleLarge,
+                color = orange,
+                modifier = Modifier.padding(start = 8.dp)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .background(Color.White)
+                .fillMaxHeight(0.13f),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Text(
+                text = "omie",
+                fontFamily = katibehFontFamily,
+                style = Typography.titleMedium,
+                color = orange
+            )
+        }
+    }
+}
