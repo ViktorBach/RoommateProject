8f83a82745b61c431e5d184c216897a65ef33eb7
BirkLauritzen <birklauritzen@gmail.com>
2024-05-02 12:06:12 +0200

    Create SleepingButton.kt

---
+++ app/src/main/java/com/example/roommateproject/FrontPage/Components/SleepingButton.kt
@@ -0,0 +1,44 @@
+package com.example.roommateproject.FrontPage.Components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.roommateproject.ui.theme.Typography
+import com.example.roommateproject.ui.theme.jaldiBoldFontFamily
+import com.example.roommateproject.ui.theme.lightYellow
+import com.example.roommateproject.ui.theme.white
+
+@Composable
+fun SleepingButton() {
+    Box(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxHeight(0.3f)
+            .fillMaxWidth(0.5f),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = {}, //insert notification that user is going to bed
+            colors = ButtonDefaults.buttonColors(
+                lightYellow
+            ), modifier = Modifier.padding(start = 25.dp)
+        ) {
+            Text(
+                text = "I'm sleeping ",
+                fontFamily = jaldiBoldFontFamily,
+                style = Typography.labelSmall,
+                color = white
+            )
+        }
+    }
+}
