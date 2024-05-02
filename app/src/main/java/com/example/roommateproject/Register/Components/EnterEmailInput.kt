b0b26279c9175bc6aff04cfd4225fc70c417040b
BirkLauritzen <birklauritzen@gmail.com>
2024-05-02 15:03:32 +0200

    Create EnterEmailInput.kt

---
+++ app/src/main/java/com/example/roommateproject/Register/Components/EnterEmailInput.kt
@@ -0,0 +1,18 @@
+package com.example.roommateproject.Register.Components
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roommateproject.Register.BoxLayout
+import com.example.roommateproject.Register.RegisterViewModel
+
+@Composable
+fun EnterEmailInput (navigateRoomLogin: () -> Unit) {
+
+    val registerViewModel = viewModel<RegisterViewModel>()
+    BoxLayout(
+        value = registerViewModel.email,
+        onValueChange = { newValue -> registerViewModel.onEmailChange(newValue) },
+        labelText = "Enter email",
+        height = 0.17f
+    )
+}
