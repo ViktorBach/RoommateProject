7a01b8ec96aea9cb77987ad98c704ba5deb0df08
BirkLauritzen <birklauritzen@gmail.com>
2024-05-02 15:03:41 +0200

    Create EnterUsernameInput.kt

---
+++ app/src/main/java/com/example/roommateproject/Register/Components/EnterUsernameInput.kt
@@ -0,0 +1,18 @@
+package com.example.roommateproject.Register.Components
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roommateproject.Register.BoxLayout
+import com.example.roommateproject.Register.RegisterViewModel
+
+@Composable
+fun EnterUsernameInput (navigateRoomLogin: () -> Unit) {
+
+    val registerViewModel = viewModel<RegisterViewModel>()
+    BoxLayout(
+        value = registerViewModel.username,
+        onValueChange = { newValue -> registerViewModel.onUsernameChange(newValue) },
+        labelText = "Enter username",
+        height = 0.23f
+    )
+}
