bbaeea2af9110681c18d6e573c5e5b0c58f52bb8
BirkLauritzen <birklauritzen@gmail.com>
2024-05-07 00:37:12 +0200

    Creating NotificaitonUtility

---
+++ app/src/main/java/com/example/roommateproject/SharedComponents/NotificationUtility.kt
@@ -0,0 +1,55 @@
+import android.Manifest
+import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import com.example.roommateproject.R
+
+const val MY_CHANNEL_ID = "my_channel_id"
+const val PERMISSION_REQUEST_CODE = 123
+
+fun createNotificationChannel(context: Context) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = context.getString(R.string.channel_name)
+        val descriptionText = context.getString(R.string.channel_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(MY_CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+}
+
+fun sendNotification(message: String, context: Context) {
+    val builder = NotificationCompat.Builder(context, MY_CHANNEL_ID)
+        .setSmallIcon(R.drawable.ic_notification)
+        .setContentTitle("New Notification")
+        .setContentText(message)
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+    with(NotificationManagerCompat.from(context)) {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission is granted, send the notification
+            notify((System.currentTimeMillis() and 0xFFFFFFF).toInt(), builder.build())
+        } else {
+            // Permission is not granted, request the permission
+            ActivityCompat.requestPermissions(
+                context as Activity,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+}
