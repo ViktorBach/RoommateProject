a0879790f0412c4079019a5a105ae79dd8a2219a
SofieAmalie44 <sofieamaliethorlund@gmail.com>
2024-05-15 13:18:42 +0200

    New formatted timestamp and layout inside the newsTap

--- app/src/main/java/com/example/roommateproject/FrontPage/Components/NewsTab.kt
+++ app/src/main/java/com/example/roommateproject/FrontPage/Components/NewsTab.kt
@@ -16,9 +16,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,6 +42,8 @@ import java.time.format.DateTimeFormatter
 @Composable
 fun NewsTab() {
     val accountService: AccountService = AccountService();
+
+
     Box(
         modifier = Modifier
             .clip(shape = RoundedCornerShape(30.dp)),
@@ -53,7 +61,7 @@ fun NewsTab() {
                 modifier = Modifier
                     .background(Color.White)
                     .fillMaxHeight(0.9f)
-                    .verticalScroll(ScrollState(1),true )
+                    .verticalScroll(ScrollState(1), true)
                     .fillMaxWidth(0.95f),
                 contentAlignment = Alignment.TopCenter
             ) {
@@ -61,12 +69,24 @@ fun NewsTab() {
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Row(modifier = Modifier
-                        .align(Alignment.CenterHorizontally),
+                    Row(
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally),
                     ) {
-                        Text(text = "News", color = earthyBrown, fontFamily = jaldiFontFamily, fontSize = 20.sp)
+                        Text(
+                            text = "News",
+                            color = earthyBrown,
+                            fontFamily = jaldiFontFamily,
+                            fontSize = 25.sp
+                        )
                     }
-                Text(text = "${AccountService.currentEvents.map { e -> e.eventType.eventText + " - " + e.timeStamp + "\r\n" }}")
+
+                    val eventString = AccountService.currentEvents.joinToString("\n\n") { " ${it.eventType.eventText}      ${it.timeStamp}" }
+                    Text(text = eventString,
+                        fontFamily = jaldiFontFamily,
+                        color = Color.DarkGray,
+                        fontSize = 16.sp
+                        )
                 }
             }
         }
