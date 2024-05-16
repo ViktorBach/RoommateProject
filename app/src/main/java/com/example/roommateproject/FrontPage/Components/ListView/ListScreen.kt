37d1c57a30fa4aac708645e57a48db242987ef57
ViktorBach <vmb0107@gmail.com>
2024-05-16 14:37:02 +0200

    added list to frontpage to github

---
+++ app/src/main/java/com/example/roommateproject/FrontPage/Components/ListView/ListScreen.kt
@@ -0,0 +1,75 @@
+package com.example.roommateproject.FrontPage.Components.ListView
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.remember
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+
+@Composable
+fun ListScreenStateful(listViewModel: ListViewModel = viewModel()) {
+    val lists: List<ShoppingList> = remember { listViewModel.tasks }
+    val input: State<String?> = remember { listViewModel.inputState }
+
+    ListsScreen(
+        lists = lists,
+        input = input.value,
+        onTaskClick = { listViewModel.toggleTaskCompleted(it.id) },
+        onInputChange = { listViewModel.onInputChange(it) },
+        onSubmitInput = { listViewModel.createTask(it) }
+    )
+}
+
+@Composable
+fun ListsScreen(
+    lists: List<ShoppingList>,
+    input: String?,
+    onTaskClick: (ShoppingList) -> Unit,
+    onInputChange: (String) -> Unit,
+    onSubmitInput: (String) -> Unit
+) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        BasicTextField(
+            value = input ?: "",
+            onValueChange = onInputChange,
+            modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+        )
+        Text("Add Task", modifier = Modifier.clickable { onSubmitInput(input ?: "") })
+        Spacer(modifier = Modifier.height(16.dp))
+        lists.forEach { task ->
+            Text(
+                text = if (task.completed) "✓ ${task.title}" else task.title,
+                modifier = Modifier.clickable { onTaskClick(task) }
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTasksScreen() {
+    ListsScreen(
+        lists = ListViewModel.sampleTasks,
+        input = null,
+        onTaskClick = {},
+        onInputChange = {},
+        onSubmitInput = {}
+    )
+}
