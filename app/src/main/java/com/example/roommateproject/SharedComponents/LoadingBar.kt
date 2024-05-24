1c5754980c25df184b56af36ae3d3d7e1bcabe18
natazja <natazjadahl@gmail.com>
2024-05-24 15:35:53 +0200

    Drawer rykker nu ikke til NewsTab, når drawer åbnes + loadingbar mellem register/login til frontpage

---
+++ app/src/main/java/com/example/roommateproject/SharedComponents/LoadingBar.kt
@@ -0,0 +1,31 @@
+package com.example.roommateproject.SharedComponents
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.roommateproject.ui.theme.lightBlue
+
+@Composable
+fun CustomLoadingBar() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0x80000000)) // Semi-transparent background
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(100.dp)
+                .background(Color.White, shape = RoundedCornerShape(16.dp))
+        ) {
+            CircularProgressIndicator(color = lightBlue)
+        }
+    }
+}
